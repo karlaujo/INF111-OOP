aac04b67a439d0db3ac6e12f92f76e00cface6d6
KARLAU <KARLAU@DESKTOP-L436G57>
2022-01-24 00:28:17 -0500

    2022-01-24: 12:28AM

--- Labo3/src/com/moumene/labo3/Exercice1.java
+++ Labo3/src/com/moumene/labo3/Exercice1.java
@@ -29,14 +29,14 @@ public class Exercice1 {
 		double prix2 = clavier.nextDouble();
 		double prix3 = clavier.nextDouble();
 		
+		//Ajout de la classe de gestion des données
+		GestionnaireProduits.initialiserProduit(p1, nom1, prix1);
+		GestionnaireProduits.initialiserProduit(p2, nom2, prix2);
+		GestionnaireProduits.initialiserProduit(p3, nom3, prix3);
+		
 		//Affichage données
-		GestionnaireProduits.initialiserProduit(p1, nom1, prix1);
 		System.out.println(p1.numero+" "+p1.nom+" "+p1.prix+"");
-		
-		GestionnaireProduits.initialiserProduit(p1, nom1, prix1);
-		System.out.println(p1.numero+" "+p1.nom+" "+p1.prix+"");
-		
-		GestionnaireProduits.initialiserProduit(p1, nom1, prix1);
-		System.out.println(p1.numero+" "+p1.nom+" "+p1.prix+"");
+		System.out.println(p2.numero+" "+p2.nom+" "+p2.prix+"");
+		System.out.println(p3.numero+" "+p3.nom+" "+p3.prix+"");
 	}
 }
